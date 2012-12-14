93896736e09163dcdb1cad1f4dc165898e007e63
Toivo Henningsson <toivo.h.h@gmail.com>
2012-12-14 22:11:37 +0100

    Move out @immutable to own file

---
+++ src/immutable.jl
@@ -0,0 +1,33 @@
+
+macro immutable(ex)
+    code_immutable(ex)
+end
+
+function code_immutable(ex)
+    @expect is_expr(ex, :type, 2)
+    sig, body = ex.args    
+    typename = (is_expr(sig, :(<:), 2) ? sig.args[1] : sig)::Symbol 
+    fields, types, sigs = Symbol[], {}, {}
+    for arg in body.args
+        if isa(arg, Symbol)
+            push(fields, arg)
+            push(types,  quot(Any))
+            push(sigs, arg)
+        elseif is_expr(arg, :(::), 2)
+            push(fields, arg.args[1])
+            push(types,  arg.args[2])            
+            push(sigs, arg)
+        end
+    end
+    
+    instances = Dict()
+    ast=esc(quote
+        type $sig
+            $(body.args...)
+            $typename($(sigs...)) = @get!($(quot(instances)), ($(fields...),),
+                                          new($(fields...)))
+        end
+    end)
+#    @show ast
+    ast
+end
