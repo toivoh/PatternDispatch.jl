fe55aab8ad69b18041ca86385265752692dac8ad
Toivo Henningsson <toivo@control.lth.se>
2012-12-17 16:20:45 +0100

    Restructure CodeMatch

--- src/CodeMatch.jl
+++ src/CodeMatch.jl
@@ -6,9 +6,9 @@ export code_match
 
 type Ctx
     code::Vector
-    values::Dict{Value,Symbol}
+    results::Dict{Node,Any}
     bound::Set{Symbol}
-    Ctx() = new({}, Dict{Value,Symbol}(), Set{Symbol}())
+    Ctx() = new({}, Dict{Node,Any}(), Set{Symbol}())
 end
 
 emit(c::Ctx, ex) = (push(c.code, ex); nothing)
@@ -16,34 +16,39 @@ emit_guard(c::Ctx, ex) = emit(c, :( if !$ex; return (false,nothing); end ))
 
 function code_match(p::Pattern)
     c = Ctx()
-    for g in values(p.guards); code_match(c, g); end
-    for b in p.bindings;       code_match(c, b); end
+    for g in values(p.guards); evaluate(c, g); end
+    for b in p.bindings;       evaluate(c, b); end
     quote; $(c.code...); end
 end
 
-function code_match(c::Ctx, v::Value)
-    if has(c.values, v)
-        c.values[v]
+function evaluate(c::Ctx, node::Node)
+    if has(c.results, node)
+        c.results[node]
     else
-        val = gensym("v")
-        emit(c, :( $val = $(code_val(c, v)) ))
-        c.values[v] = val
+        ex = code_match(c, node)
+        if (isa(ex, Symbol) || ex === nothing)
+            c.results[node] = ex
+        else
+            val = gensym("v")
+            emit(c, :( $val = $ex ))
+            c.results[node] = val            
+        end
     end
 end
 
-code_val(c::Ctx, v::Arg)      = argsym
-code_val(c::Ctx, v::TupleRef) = :( $(code_match(c,v.arg))[$(v.index)] )
+code_match(c::Ctx, v::Arg)      = argsym
+code_match(c::Ctx, v::TupleRef) = :( $(evaluate(c,v.arg))[$(v.index)] )
 
 function code_match(c::Ctx, g::Bind)
     if has(c.bound, g.name)
-        emit_guard(c, :( is($(code_match(c,g.arg)), $(g.name)) ))
+        emit_guard(c, :( is($(evaluate(c,g.arg)), $(g.name)) ))
     else
-        emit(c, :( $(g.name) = $(code_match(c,g.arg)) ))
+        emit(c, :( $(g.name) = $(evaluate(c,g.arg)) ))
     end
 end
 code_match(c::Ctx, g::Guard) = emit_guard(c, code_pred(c, g))
 
-code_pred(c::Ctx,g::Egal) = :(is( $(code_match(c, g.arg)), $(quot(g.value))))
-code_pred(c::Ctx,g::Isa)  = :(isa($(code_match(c, g.arg)), $(quot(g.typ  ))))
+code_pred(c::Ctx,g::Egal) = :(is( $(evaluate(c, g.arg)), $(quot(g.value))))
+code_pred(c::Ctx,g::Isa)  = :(isa($(evaluate(c, g.arg)), $(quot(g.typ  ))))
 
 end # module
