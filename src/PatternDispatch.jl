a1f5ca061f3a535bd18b18fb60ec71423380526a
Toivo Henningsson <toivo@control.lth.se>
2012-12-17 14:44:29 +0100

    Pull out @pattern into module Macros.jl

--- src/PatternDispatch.jl
+++ src/PatternDispatch.jl
@@ -10,7 +10,9 @@ include(find_in_path("PatternDispatch/src/Immutable.jl"))
 include(find_in_path("PatternDispatch/src/Graph.jl"))
 include(find_in_path("PatternDispatch/src/Recode.jl"))
 include(find_in_path("PatternDispatch/src/CodeMatch.jl"))
+include(find_in_path("PatternDispatch/src/Macros.jl"))
 using Graph, Recode, CodeMatch
+using Macros
 
 
 # ==== simplify ===============================================================
@@ -74,74 +76,6 @@ end
 
 
 
-# ==== MethodTable ============================================================
-
-type MethodTable
-    name::Symbol
-    methods::Vector{Function}
-    MethodTable(name::Symbol) = new(name, Function[])
-end
-
-add(mt::MethodTable, m::Function) = (push(mt.methods, m); nothing)
-
-function dispatch(mt::MethodTable, args::Tuple)
-    for m in mt.methods
-        matched, result = m(args)
-        if matched; return result; end
-    end
-    error("No matching method found for pattern function $(mt.name)")
-end
-
-
-function create_method(p::Pattern, body)
-    code = code_match(p)
-    @eval $argsym->begin
-        $(code)
-        (true, $body)
-    end
-end
-
-# ==== @pattern ===============================================================
-
-const method_tables = Dict{Function, MethodTable}()
-
-macro pattern(ex)
-    code_pattern(ex)
-end
-
-function code_pattern(ex)
-    sig, body = split_fdef(ex)
-    @expect is_expr(sig, :call)
-    fname, args = sig.args[1], sig.args[2:end]
-    psig = :($(args...),)
-    p_ex = recode(psig)
-    
-    f = esc(fname)
-    quote       
-        wasbound = try
-            f = $f
-            true
-        catch e
-            false
-        end
-
-        if !wasbound
-            mt = MethodTable($(quot(fname)))
-            const $f = (args...)->dispatch(mt, args)
-            method_tables[$f] = mt
-            println($("$fname was unbound"))
-        else
-            if !has(method_tables, $f)
-                error($("$fname is not a pattern function"))
-            end
-            mt = method_tables[$f]
-            println($("$fname was a pattern function"))
-        end
-
-        method = create_method($p_ex, $(quot(body)))
-        add(mt, method)
-    end
-end
 
 macro qpat(ex)
     recode(ex)
