95d958fee73bd84d2d4e157542b4d124b559539b
Toivo Henningsson <toivo@control.lth.se>
2012-12-17 14:35:55 +0100

    Pull out recode into module Recode.jl

--- src/PatternDispatch.jl
+++ src/PatternDispatch.jl
@@ -8,52 +8,8 @@ export @pattern, @qpat, @spat, simplify, unbind
 
 include(find_in_path("PatternDispatch/src/Immutable.jl"))
 include(find_in_path("PatternDispatch/src/Graph.jl"))
-using Graph
-
-
-# ==== recode: function signature -> Pattern creating AST =====================
-
-type Recode
-    code::Vector
-    guards::Vector
-    Recode() = new({}, {})
-end
-
-function recode(ex)
-    r = Recode()
-    recode(r, quot(Arg()), ex)
-    quote
-        $(r.code...)
-        Pattern(Guard[$(r.guards...)])
-    end
-end
-
-recode(c::Recode, arg, ex)         = push(c.guards, :(Egal($arg, $(quot(ex)))))
-recode(c::Recode, arg, ex::Symbol) = push(c.guards, :(Bind($arg, $(quot(ex)))))
-function recode(c::Recode, arg, ex::Expr)
-    head, args = ex.head, ex.args
-    nargs = length(args)
-    if head === :(::)
-        @assert 1 <= nargs <= 2
-        if nargs == 1
-            push(c.guards, :( Isa($arg, $(esc(args[1]))) ))
-        else
-            push(c.guards, :( Isa($arg, $(esc(args[2]))) ))
-            recode(c, arg, args[1])
-        end
-    elseif head === :tuple
-        push(c.guards, :( Isa($arg, $(quot(NTuple{nargs,Any}))) ))
-        for (k, p) in enumerate(args)
-            node = gensym("e$k")
-            push(c.code, :( $node = TupleRef($arg, $k) ))
-            recode(c, node, p)
-        end
-    elseif head === :call && args[1] == :~
-        for p in args[2:end]; recode(c, arg, p); end
-    else
-        error("recode: unimplemented: ex = $ex")
-    end
-end
+include(find_in_path("PatternDispatch/src/Recode.jl"))
+using Graph, Recode
 
 
 # ==== simplify ===============================================================
