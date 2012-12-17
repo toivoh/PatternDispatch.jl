d779d4a4b6469bb939c0443e0903caa452441581
Toivo Henningsson <toivo@control.lth.se>
2012-12-17 14:40:22 +0100

    Pull out code_match into module CodeMatch.jl

--- src/PatternDispatch.jl
+++ src/PatternDispatch.jl
@@ -9,7 +9,8 @@ export @pattern, @qpat, @spat, simplify, unbind
 include(find_in_path("PatternDispatch/src/Immutable.jl"))
 include(find_in_path("PatternDispatch/src/Graph.jl"))
 include(find_in_path("PatternDispatch/src/Recode.jl"))
-using Graph, Recode
+include(find_in_path("PatternDispatch/src/CodeMatch.jl"))
+using Graph, Recode, CodeMatch
 
 
 # ==== simplify ===============================================================
@@ -71,50 +72,6 @@ end
 <(p::Pattern, q::Pattern)  = q >  p
 
 
-# ==== code_match: Pattern -> matching code ===================================
-
-type Ctx
-    code::Vector
-    values::Dict{Value,Symbol}
-    bound::Set{Symbol}
-    Ctx() = new({}, Dict{Value,Symbol}(), Set{Symbol}())
-end
-
-emit(c::Ctx, ex) = (push(c.code, ex); nothing)
-emit_guard(c::Ctx, ex) = emit(c, :( if !$ex; return (false,nothing); end ))
-
-function code_match(p::Pattern)
-    c = Ctx()
-    for g in p.guards
-        code_match(c, g)
-    end
-    quote; $(c.code...); end
-end
-
-function code_match(c::Ctx, v::Value)
-    if has(c.values, v)
-        c.values[v]
-    else
-        val = gensym("v")
-        emit(c, :( $val = $(code_val(c, v)) ))
-        c.values[v] = val
-    end
-end
-
-code_val(c::Ctx, v::Arg)      = argsym
-code_val(c::Ctx, v::TupleRef) = :( $(code_match(c,v.arg))[$(v.index)] )
-
-function code_match(c::Ctx, g::Bind)
-    if has(c.bound, g.name)
-        emit_guard(c, :( is($(code_match(c,g.arg)), $(g.name)) ))
-    else
-        emit(c, :( $(g.name) = $(code_match(c,g.arg)) ))
-    end
-end
-code_match(c::Ctx, g::Guard) = emit_guard(c, code_pred(c, g))
-
-code_pred(c::Ctx,g::Egal) = :(is( $(code_match(c, g.arg)), $(quot(g.value))))
-code_pred(c::Ctx,g::Isa)  = :(isa($(code_match(c, g.arg)), $(quot(g.typ  ))))
 
 
 # ==== MethodTable ============================================================
