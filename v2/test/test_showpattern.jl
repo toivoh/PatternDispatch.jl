cc95502d6866df908023a47f8fd61489a1758e4e
Toivo Henningsson <toivo.h.h@gmail.com>
2012-12-20 22:15:49 +0100

    Copy test_showpattern to v2

---
+++ v2/test/test_showpattern.jl
@@ -0,0 +1,28 @@
+include(find_in_path("PatternDispatch/v2/PatternDispatch.jl"))
+
+module TestShowPattern
+using PatternDispatch
+
+@show (@qpat 1)
+@show (@qpat 1::Int)
+@show (@qpat 1::Float64)
+
+println()
+@show (@qpat x::Any)
+@show (@qpat ::Any)
+@show (@qpat ::None)
+@show (@qpat (x::None,y::Int))
+
+println()
+@show (@qpat x)
+@show (@qpat ::Int)
+@show (@qpat x::Int)
+@show (@qpat x~y)
+@show (@qpat x~y::Int)
+
+println()
+@show (@qpat (x,y))
+@show (@qpat (x,y)::Tuple)
+@show (@qpat z~(x::Int,1))
+
+end 
