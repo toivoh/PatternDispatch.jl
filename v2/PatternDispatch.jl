7ddb0c2cc0b6622c8d15b7053ae3af114a280348
Toivo Henningsson <toivo.h.h@gmail.com>
2012-12-20 22:13:22 +0100

    Adapt Recode to v2

--- v2/PatternDispatch.jl
+++ v2/PatternDispatch.jl
@@ -3,8 +3,15 @@ load("Debug.jl")
 
 module PatternDispatch
 using Toivo, Debug
+export @qpat
 
 include(find_in_path("PatternDispatch/src/Immutable.jl"))
 include(find_in_path("PatternDispatch/v2/Patterns.jl"))
+include(find_in_path("PatternDispatch/v2/Recode.jl"))
+using Patterns, Recode
+
+macro qpat(ex)
+    recode(ex)
+end
 
 end # module
