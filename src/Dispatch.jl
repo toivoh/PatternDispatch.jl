e124cecc9c4de994a48c526759b004b45f2f746e
Toivo Henningsson <toivo.h.h@gmail.com>
2012-12-25 08:33:01 +0100

    Transplant cooperative dispatch into Dispatch.jl, make it recompile less often

--- src/Dispatch.jl
+++ src/Dispatch.jl
@@ -2,98 +2,97 @@
 module Dispatch
 import Base.add
 import Nodes
-using PartialOrder, Patterns, DecisionTree, Toivo
+using PartialOrder, Patterns, DecisionTree, Encode, Toivo
 export MethodTable, Method, methodsof
 
 type MethodTable
     name::Symbol
     top::MethodNode
     f::Function
+    compiled::Bool
 
     function MethodTable(name::Symbol) 
         f = eval(:(let
                 $name(args...) = error("No matching pattern method found")
                 $name
             end))
-        new(name, MethodNode(nomethod), f)
+        mt = new(name, MethodNode(nomethod), f, false)
+        eval(:(let
+                const f = $f
+                function f(args...)
+                    create_dispatch($(quot(mt)))
+                    $(quot(mt)).f(args...)
+                end
+            end))
+        mt
     end
 end
 
 function add(mt::MethodTable, m::Method)
     insert!(mt.top, MethodNode(m))
     # todo: only when necessary
-    create_dispatch(mt)
-#    create_dispatch2(mt)
-end
-
-function create_dispatch(mt::MethodTable)
-    code = code_dispatch(mt.top)
-    # todo: which eval?
-    eval(:(let
-            const f = $(mt.f)
-            f($(Nodes.argsym)...) = $code
-        end))
+    if mt.compiled;  create_dispatch(mt)  end
 end
 
 function methodsof(mt::MethodTable)
     ms = subDAGof(mt.top)
-#    if mt.top.value === nomethod;  del(ms, mt.top);  end # exclude nomethod
     [m.value for m in ms]
 end
 
-# ---- Experimental cooperative dispatch ----
+function create_dispatch(mt::MethodTable)
+    mt.compiled = true
+    eval(:(let
+            const f = $(mt.f)
+            f(args...) = error("No matching pattern method found")
+        end))
 
-function create_dispatch2(mt::MethodTable)
-    ms = subDAGof(mt.top)
-    if mt.top.value.body === nothing
-        del(ms, mt.top) # don't take the signature from nomethod
-    end
-    tups = Set{Tuple}({julia_signature_of(m.value.sig) for m in ms}...)
-    for tup in tups
-        create_dispatch(mt, ms, tup)
-    end
+    methods = methodsof(mt)
+    actual_methods = filter(m->(m != nomethod), methods)
+    hullTs = Set{Tuple}(Tuple[m.hullT for m in actual_methods]...)
+
+    for hullT in hullTs;  create_dispatch(mt, methods, hullT);  end
 end
 
-function create_dispatch(mt::MethodTable, ms::Set{MethodNode}, tup::Tuple)
-    # create new method nodes
-    intent = julia_intension(tup)
-    #methods = {m.value & intent for m in ms}
-    methods = {filter(m->(!(m.sig.intent === naught)), 
-                      {m.value & intent for m in ms})...}
+ltT(S,T) = !(S==T) && (S<:T)
+function create_dispatch(mt::MethodTable, methods::Vector{Method},hullT::Tuple)
+    top = copyDAG(mt.top)
 
-    # create new method DAG
-    top = MethodNode(Method(Pattern(intent), nothing))
-    for m in methods;  insert!(top, MethodNode(m))  end
+    # filter out too specific methods
+    keep = Set{Method}(filter(m->!ltT(m.hullT, hullT), methods)...)
+    @assert !isempty(keep)
+    raw_filter!(top, keep)
+
+    # filter out non-questions
+    hull = intension(hullT)
+    top = simplify!(top, hull)
+
+#     @show hullT
+#     @show tuple([node.value.sig for node in subDAGof(top)]...)
+#     @show top.value.sig
+#     println()
     
     # create dispatch code using given assumptions and args
+    # todo: move some of this into DecisionTree
     results = ResultsDict()
-    for pred in guardsof(intent);  preguard!(results, Guard(pred));  end
-    argsyms = {gensym("arg") for k=1:length(tup)}
+    for pred in guardsof(hull);  preguard!(results, Guard(pred));  end
+    argsyms = {gensym("arg") for k=1:length(hullT)}
     for (k, argsym) in enumerate(argsyms)
-        provide!(results, Nodes.tupref(Nodes.argnode, k), Nodes.argsym)
+        provide!(results, Nodes.tupref(Nodes.argnode, k), argsym)
     end
 
     code = code_dispatch(top, results)
-    args = {:($argsym::$(quot(T))) for (argsym,T) in zip(argsyms,tup)}
+    args = {:($argsym::$(quot(T))) for (argsym,T) in zip(argsyms,hullT)}
     fdef = quote
         function f($(args...))
             $code
         end
     end
-    @show fdef
+#   @show fdef
 
     eval(:(let
             const f = $(mt.f)
             f($(args...)) = $code
         end))
-
-    # todo: reuse bodies!    
-    # todo: respect declaration order among collisions?
-    
 end
-
-
-# ---- Decision Tree ----------------------------------------------------------
-
 
 end # module
