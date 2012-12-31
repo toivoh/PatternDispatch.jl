db290720a90ca93ab61bbb8682923284281ad3ce
Toivo Henningsson <toivo.h.h@gmail.com>
2012-12-31 09:01:22 +0100

    Locate source files using julia_pkgdir()

--- src/PatternDispatch.jl
+++ src/PatternDispatch.jl
@@ -1,15 +1,15 @@
 module PatternDispatch
 export @pattern, show_dispatch
 
-include(find_in_path("PatternDispatch/src/Meta.jl"))
-include(find_in_path("PatternDispatch/src/PartialOrder.jl"))
-include(find_in_path("PatternDispatch/src/Immutable.jl"))
-include(find_in_path("PatternDispatch/src/Patterns.jl"))
-include(find_in_path("PatternDispatch/src/Nodes.jl"))
-include(find_in_path("PatternDispatch/src/Recode.jl"))
-include(find_in_path("PatternDispatch/src/Encode.jl"))
-include(find_in_path("PatternDispatch/src/DecisionTree.jl"))
-include(find_in_path("PatternDispatch/src/Dispatch.jl"))
+include(julia_pkgdir()*"/PatternDispatch/src/Meta.jl")
+include(julia_pkgdir()*"/PatternDispatch/src/PartialOrder.jl")
+include(julia_pkgdir()*"/PatternDispatch/src/Immutable.jl")
+include(julia_pkgdir()*"/PatternDispatch/src/Patterns.jl")
+include(julia_pkgdir()*"/PatternDispatch/src/Nodes.jl")
+include(julia_pkgdir()*"/PatternDispatch/src/Recode.jl")
+include(julia_pkgdir()*"/PatternDispatch/src/Encode.jl")
+include(julia_pkgdir()*"/PatternDispatch/src/DecisionTree.jl")
+include(julia_pkgdir()*"/PatternDispatch/src/Dispatch.jl")
 using Meta, Patterns, Nodes, Recode, Dispatch
 import Dispatch.show_dispatch
 
