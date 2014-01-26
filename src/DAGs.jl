55af9e96446028194931f6c2447bccc9007ccc06
Toivo Henningsson <toivo.h.h@gmail.com>
2014-01-26 09:27:07 +0100

    Add first attempt at PatternDAG, with more simplifications

--- src/DAGs.jl
+++ src/DAGs.jl
@@ -5,7 +5,7 @@ export TGof
 
 using ..Common.Head
 import ..Common
-using ..Ops: Calc, EgalGuard, TypeGuard, Tof
+using ..Ops: Calc, EgalGuard, TypeGuard, Tof, Never, Source, valueof
 import ..Common: emit!, calc!, meet
 
 
@@ -168,6 +168,46 @@ end
 
 tgkey(node::Node) = keyof(TypeGuard(Any), node)
 TGof(g, node::Node) = (key = tgkey(primary_rep(node)); haskey(g, key) ? Tof(headof(g[key])) : Any)
+
+
+immutable PatternDAG
+    g::DAG
+    PatternDAG() = new(DAG())
+end
+
+Base.haskey(  g::PatternDAG, key) = haskey(g.g, key)
+Base.getindex(g::PatternDAG, key) = g.g[key]
+
+emit!(g::PatternDAG, head::Head, args::Node...) = (emit!(g.g, head, args!); simplify!(g); nothing)
+function calc!(g::PatternDAG, head::Head, args::Node...)
+    node = calc!(g.g, head, args!)
+    simplify!(g)
+    primary_rep(node) # consider: do we want to take the primary_rep here? active_rep?
+end
+
+calc!(g::PatternDAG, head::Source, args::Node...) = error("Source nodes take no args")
+function calc!(g::PatternDAG, head::Source)
+    node = calc!(g.g, head)
+    emit!(g.g, TypeGuard(typeof(valueof(head))), node)
+    # NB: below duplicated from common calc! How to avoid?
+    simplify!(g)
+    primary_rep(node) # consider: do we want to take the primary_rep here? active_rep?
+end
+
+never!(g::PatternDAG) = (emit!(g, Never()); nothing)
+
+visit!(g::PatternDAG, node::Node) = nothing
+visit!(g::PatternDAG, node::Node{TypeGuard}) = (if Tof(headof(node)) == None; never!(g); end)
+# NB: assumes all nodes that become secondary end up in updated
+visit!(g::PatternDAG, node::Node{Source}) = (if !primary_rep(node) === node; never!(g); end)
+
+function simplify!(g::PatternDAG)
+    while !isempty(g.g.updated)
+        node = pop!(g.g.updated)
+        if !iskind(active_node, node); continue; end
+        visit!(g, node)
+    end    
+end
 
 
 end # module
