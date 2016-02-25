d6488df07807313c7570ea5d482bb21b567866ca
Toivo Henningsson <toivo.h.h@gmail.com>
2016-02-25 20:38:28 +0100

    Add othertests.jl to run all printing tests

---
+++ test/othertests.jl
@@ -0,0 +1,7 @@
+include("test_ambwarning.jl")
+include("test_big_dtree.jl")
+include("test_examples.jl")
+include("test_julia_signature.jl")
+include("test_named_nodes.jl")
+include("test_show_dispatch.jl")
+include("test_showpattern.jl")
